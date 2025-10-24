8abadcb0e161057e3b3c55efd2152fff89f9aa7a
G-Essary <essary.619432@swl.k12.oh.us>
2025-10-23 23:01:26 -0400

    this took so (crapping???) long

--- src/Craps.java
+++ src/Craps.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Craps {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
